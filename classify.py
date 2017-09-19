91224d2571695575029afdcb3c151164988c82a8
Anusha Rao <anusha2701@gmail.com>
2017-09-20 00:34:43 +0200

    added xgboost

--- classify.py
+++ classify.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
+import xgboost as xgb
 
 X_test = np.load('X_test.npy')
 y_test = np.load('y_test.npy')
@@ -13,7 +14,10 @@ y_train = np.load('y_train.npy')
                         # algorithm="SAMME.R")
 
 # clf = GaussianNB()
-clf = RandomForestClassifier()
+# clf = RandomForestClassifier()
+
+clf = xgb.XGBClassifier(max_depth=3, n_estimators=300, learning_rate=0.05)
+
 clf.fit(X_train, y_train)
 scores = clf.score(X_test, y_test)
 print(scores)
